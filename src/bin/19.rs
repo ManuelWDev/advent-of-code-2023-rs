21706b074f464a9bd5c34611cf82985cfa8ba4eb
Manuel W <manuel.wolz@stud-mail.uni-wuerzburg.de>
2023-12-19 17:29:57 +0100

    add day 19 part 2 solution
    also refactors part1 to use the same codebase

--- src/bin/19.rs
+++ src/bin/19.rs
@@ -1,4 +1,5 @@
 use std::collections::HashMap;
+use std::ops::Range;
 advent_of_code::solution!(19);
 
 #[cfg(windows)]
@@ -6,33 +7,51 @@ const EMPTY_LINE: &'static str = "\r\n\r\n";
 #[cfg(not(windows))]
 const EMPTY_LINE: &'static str = "\n\n";
 
-pub fn part_one(input: &str) -> Option<u32> {
+pub fn part_one(input: &str) -> Option<u64> {
     let (workflows, data) = parse_input(input);
-    let mut sum = 0;
-    for data in data {
-        let mut workflow = workflows.get("in").unwrap();
-        loop {
-            let result = workflow.get_next_workflow(&data);
-
-            if result == "A" {
-                sum += data.sum();
-                break;
-            }
-            if result == "R" {
-                break;
-            }
-
-            workflow = workflows.get(result).unwrap();
-        }
-
-    }
-
-
-    Some(sum)
+    Some(calculate_sum(workflows, data, &|data| data.start_sum()))
 }
 
 pub fn part_two(input: &str) -> Option<u64> {
-    None
+    let workflows = parse_workflows(input.split(EMPTY_LINE).next().unwrap());
+    let data = vec![Data {
+        data: [
+            1..4001, // x
+            1..4001, // m
+            1..4001, // a
+            1..4001, // s
+        ],
+    }];
+    Some(calculate_sum(workflows, data, &|data| data.field_count()))
+}
+
+fn calculate_sum(
+    workflows: HashMap<&str, WorkFlow>,
+    data: Vec<Data>,
+    sum_function: &dyn Fn(&Data) -> u64,
+) -> u64 {
+    let mut sum = 0;
+    for data in data {
+        let mut current_datas = vec![(data, "in")];
+        while current_datas.len() > 0 {
+            let current_data = current_datas.pop().unwrap();
+            let workflow = workflows.get(current_data.1).unwrap();
+            let result = workflow.get_next_workflows(current_data.0);
+
+            for (data, target) in result {
+                if target == "A" {
+                    sum += sum_function(&data);
+                    continue;
+                }
+                if target == "R" {
+                    continue;
+                }
+
+                current_datas.push((data, target));
+            }
+        }
+    }
+    sum
 }
 
 fn parse_input(input: &str) -> (HashMap<&str, WorkFlow>, Vec<Data>) {
@@ -56,10 +75,7 @@ fn parse_workflows<'a>(input: &'a str) -> HashMap<&'a str, WorkFlow> {
 }
 
 fn parse_data(input: &str) -> Vec<Data> {
-    input
-        .lines()
-        .map(|line| line.into())
-        .collect()
+    input.lines().map(|line| line.into()).collect()
 }
 
 struct WorkFlow<'a> {
@@ -67,13 +83,22 @@ struct WorkFlow<'a> {
 }
 
 impl<'a> WorkFlow<'a> {
-    fn get_next_workflow(&self, data: &Data) -> &'a str {
+    fn get_next_workflows(&self, data: Data) -> Vec<(Data, &'a str)> {
+        let mut output_data = Vec::new();
+        let mut test_data = data;
+
         for rule in &self.rules {
-            if let Some(result) = rule.check(data) {
-                return result;
+            let (accepted, rejected) = rule.check(test_data);
+            if let Some(accepted) = accepted {
+                output_data.push(accepted);
+            }
+            if let Some(rejected) = rejected {
+                test_data = rejected;
+            } else {
+                break;
             }
         }
-        panic!("No rule found for data: {:?}", data);
+        output_data
     }
 }
 
@@ -94,14 +119,15 @@ struct Rule<'a> {
 }
 
 impl<'a> Rule<'a> {
-    fn check(&self, data: &Data) -> Option<&'a str> {
-        if self.satisfy_value.check(data, &self.check) {
-            Some(self.satisfy_result)
+    fn check(&self, data: Data) -> (Option<(Data, &'a str)>, Option<Data>) {
+        let result = self
+            .satisfy_value
+            .get_allowed_and_blocked(data, &self.check);
+        let mut accepted = None;
+        if result.0.is_some() {
+            accepted = Some((result.0.unwrap(), self.satisfy_result));
         }
-        else {
-            None
-        }
-
+        (accepted, result.1)
     }
 }
 
@@ -121,8 +147,7 @@ impl<'a> From<&'a str> for Rule<'a> {
                     satisfy_value: (left, right).into(),
                     satisfy_result: target,
                 }
-            }
-            else {
+            } else {
                 let mut inner_parts = left.split(">");
                 let left = inner_parts.next().unwrap();
                 let right = inner_parts.next().unwrap();
@@ -132,9 +157,7 @@ impl<'a> From<&'a str> for Rule<'a> {
                     satisfy_result: target,
                 }
             }
-
-        }
-        else {
+        } else {
             Rule {
                 check: Check::None,
                 satisfy_value: SatisfyValue::None,
@@ -144,27 +167,23 @@ impl<'a> From<&'a str> for Rule<'a> {
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Data {
-    x: u32,
-    m: u32,
-    a: u32,
-    s: u32,
+    data: [Range<u32>; 4],
 }
 
 impl From<&str> for Data {
     fn from(value: &str) -> Self {
         let data = &value[1..value.len() - 1];
         let mut parts = data.split(',');
+        let x = Data::get_value(parts.next().unwrap());
+        let m = Data::get_value(parts.next().unwrap());
+        let a = Data::get_value(parts.next().unwrap());
+        let s = Data::get_value(parts.next().unwrap());
         Data {
-            x: Data::get_value(parts.next().unwrap()),
-            m: Data::get_value(parts.next().unwrap()),
-            a: Data::get_value(parts.next().unwrap()),
-            s: Data::get_value(parts.next().unwrap()),
+            data: [x..(x + 1), m..(m + 1), a..(a + 1), s..(s + 1)],
         }
     }
-
-
 }
 
 impl Data {
@@ -172,8 +191,50 @@ impl Data {
         value.split('=').last().unwrap().parse().unwrap()
     }
 
-    fn sum(&self) -> u32 {
-        self.x + self.m + self.a + self.s
+    fn field_count(&self) -> u64 {
+        self.data.iter().map(|range| range.len() as u64).product()
+    }
+
+    fn start_sum(&self) -> u64 {
+        self.data.iter().map(|range| range.start as u64).sum()
+    }
+
+    fn split_at(self, value: u32, index: usize) -> (Option<Data>, Option<Data>) {
+        if self.data[index].end <= value {
+            return (None, Some(self));
+        }
+        if self.data[index].start > value {
+            return (Some(self), None);
+        }
+
+        let value = value + 1;
+
+        let mut outside_data = self.data.clone();
+        outside_data[index] = self.data[index].start..value;
+
+        let outside = Data { data: outside_data };
+
+        let mut inside_data = self.data;
+        inside_data[index] = value..inside_data[index].end;
+
+        let inside = Data { data: inside_data };
+        (Some(inside), Some(outside))
+    }
+
+    fn split_at_x(self, value: u32) -> (Option<Data>, Option<Data>) {
+        self.split_at(value, 0)
+    }
+
+    fn split_at_m(self, value: u32) -> (Option<Data>, Option<Data>) {
+        self.split_at(value, 1)
+    }
+
+    fn split_at_a(self, value: u32) -> (Option<Data>, Option<Data>) {
+        self.split_at(value, 2)
+    }
+
+    fn split_at_s(self, value: u32) -> (Option<Data>, Option<Data>) {
+        self.split_at(value, 3)
     }
 }
 
@@ -186,27 +247,35 @@ enum SatisfyValue {
 }
 
 impl SatisfyValue {
-    fn check(&self, data: &Data, check: &Check) -> bool {
+    fn get_allowed_and_blocked(&self, data: Data, check: &Check) -> (Option<Data>, Option<Data>) {
         match check {
-            Check::GreaterThan => {
-                match self {
-                    SatisfyValue::X(value) => data.x > *value,
-                    SatisfyValue::M(value) => data.m > *value,
-                    SatisfyValue::A(value) => data.a > *value,
-                    SatisfyValue::S(value) => data.s > *value,
-                    SatisfyValue::None => true,
-                }
-            }
-            Check::LessThan => {
-                match self {
-                    SatisfyValue::X(value) => data.x < *value,
-                    SatisfyValue::M(value) => data.m < *value,
-                    SatisfyValue::A(value) => data.a < *value,
-                    SatisfyValue::S(value) => data.s < *value,
-                    SatisfyValue::None => true,
-                }
+            Check::GreaterThan => match self {
+                SatisfyValue::X(value) => data.split_at_x(*value),
+                SatisfyValue::M(value) => data.split_at_m(*value),
+                SatisfyValue::A(value) => data.split_at_a(*value),
+                SatisfyValue::S(value) => data.split_at_s(*value),
+                SatisfyValue::None => (Some(data), None),
             },
-            Check::None => true,
+            Check::LessThan => match self {
+                SatisfyValue::X(value) => {
+                    let (a, b) = data.split_at_x(*value - 1);
+                    (b, a)
+                }
+                SatisfyValue::M(value) => {
+                    let (a, b) = data.split_at_m(*value - 1);
+                    (b, a)
+                }
+                SatisfyValue::A(value) => {
+                    let (a, b) = data.split_at_a(*value - 1);
+                    (b, a)
+                }
+                SatisfyValue::S(value) => {
+                    let (a, b) = data.split_at_s(*value - 1);
+                    (b, a)
+                }
+                SatisfyValue::None => (Some(data), None),
+            },
+            Check::None => (Some(data), None),
         }
     }
 }
@@ -245,6 +314,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(167409079868000));
     }
 }
